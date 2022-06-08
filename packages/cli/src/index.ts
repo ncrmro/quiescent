2df0c031fab362393c5e4f1c9c9d938b92aac29a
Nicholas Romero <ncrmro@gmail.com>
2022-06-08 15:50:48 -0500

    refactor(cli): user yargs and inquirer

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,20 +1,31 @@
-import * as yargsInteractive from "yargs-interactive";
+import yargs from "yargs";
+import * as inquirer from "inquirer";
 
-const options: yargsInteractive.Option = {
-  interactive: { default: false },
-  postTitle: {
-    type: "input",
-    describe: "Enter the name of the post",
-  },
+const createPost = async (args) => {
+  const answers = await inquirer.prompt(
+    [
+      {
+        message: "What is the title of the post?",
+        name: "title",
+        type: "string",
+      },
+    ],
+    {
+      title: args.title,
+    }
+  );
+
+  console.log(`Post title, ${answers.title}!`);
 };
 
-yargsInteractive()
-  .usage("$0 <command> [args]")
-  .interactive(options)
-  .then((result) => {
-    if (!result.postTitle) {
-      console.error("Error: A post title is required");
-      return;
-    }
-    console.log(result.postTitle);
-  });
+yargs(process.argv.splice(2))
+  .command("post", "use inquirer to prompt for your name", {}, createPost)
+  .options({
+    title: {
+      description: "The post title",
+      requiresArg: true,
+      alias: "t",
+    },
+  })
+  .strict()
+  .help("h").argv;
