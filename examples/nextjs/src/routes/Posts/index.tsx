eda4237edc4053f1b3653088c4ba79c7cea9caac
Nicholas Romero <ncrmro@gmail.com>
2022-07-27 23:34:59 -0500

    feat(examples): add link to post page

--- examples/nextjs/src/routes/Posts/index.tsx
+++ examples/nextjs/src/routes/Posts/index.tsx
@@ -1,10 +1,15 @@
-const PostsRoute: React.FC<{ posts: Record<string, string>[] }> = (props) => {
+import Link from "next/link";
+import { Post } from "../../types";
+
+const PostsRoute: React.FC<{ posts: Post[] }> = (props) => {
   console.log("AYE POSTS", props.posts);
   return (
     <div>
       Posts:
       {Object.entries(props.posts).map(([slug, post]) => (
-        <div key={slug}>SLUG {slug}</div>
+        <div key={slug}>
+          SLUG {slug} <Link href={`/posts/${slug}`}>{slug}</Link>
+        </div>
       ))}
     </div>
   );
