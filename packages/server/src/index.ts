20b47a1ae95f62ab707a0425429c0c91af00b4ee
Nicholas Romero <ncrmro@gmail.com>
2022-07-28 17:13:35 -0500

    feat(server): add get document by slug

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,9 +2,12 @@ import fs from "fs/promises";
 
 const publicDirectory = `${process.cwd()}/public`;
 
+function getDocumentDirectory(directory = "posts") {
+  return `${publicDirectory}/${directory}/`;
+}
 export async function getManifest() {
   const manifest = JSON.parse(
-    await fs.readFile(`${publicDirectory}/posts.manifest.json`, "utf8")
+    await fs.readFile(`${publicDirectory}/posts/manifest.json`, "utf8")
   );
   return {
     posts: typeof manifest.posts === "object" ? manifest.posts : {},
@@ -20,4 +23,39 @@ export async function getDocumentSlugs() {
 
 export async function getDocumentBySlug(slug: string) {
   return { slug, ...(await manifest).posts[slug] };
+}
+
+interface Document {
+  description: string;
+  content: string;
+  tags?: string[];
+}
+
+async function parseDocument(documentPath: string) {
+  if (!documentPath.includes(".md")) {
+    documentPath = `${documentPath}/document.md`;
+  }
+  const content = await fs.readFile(documentPath, "utf8");
+  const match = content.match(/---\n((\w*:) .*\n)*---/)?.[0];
+  if (match) {
+    const post: Record<string, string> = {};
+    for (const [_, k, v] of match.matchAll(/^(?<key>\w*): (?<value>.*)$/gm)) {
+      post[k] = v;
+    }
+    if (post.description) {
+      return {
+        description: post.description,
+        content: content.replace(match, ""),
+        tags: [],
+      };
+    }
+  }
+}
+
+export async function documentBySlug(slug: string) {
+  const documentDirectory = getDocumentDirectory();
+  const documentFilename = (await fs.readdir(documentDirectory)).find(
+    (fileName) => fileName.includes(slug)
+  );
+  return await parseDocument(`${documentDirectory}/${documentFilename}`);
 }
