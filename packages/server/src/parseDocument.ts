cccaa027ce7d8ff4ce0937eddba578f813f5975d
Nicholas Romero <ncrmro@gmail.com>
2022-07-29 20:33:37 -0500

    feat(server): check document filenames with regex

--- packages/server/src/parseDocument.ts
+++ packages/server/src/parseDocument.ts
@@ -10,38 +10,46 @@ export interface Document {
 }
 
 /**
- * Parse the specified markdown file into our documented format
+ * Parse the specified markdown file into our document format
  * @param documentDirectory
  * @param documentFilename
  */
-export async function parseDocument(
-  documentDirectory: string,
-  documentFilename: string
-) {
-  const [year, month, day, end] = documentFilename.split("_");
-  const slug = end.replace(".md", "");
+export async function parseDocument(documentFilename: string) {
+  const documentMatch = documentFilename.match(
+    /(?<year>\d{4})_(?<month>\d{2})_(?<date>\d{2})_(?<slug>[A-Za-z0-9-]*)[\.md|$]/
+  );
+  if (!documentMatch?.groups)
+    throw `Document file name does not match YYYY_MM_DD-document-slug ${documentFilename}`;
+  // console.log(re?.groups);
+  const { year, month, date, slug } = documentMatch.groups;
 
-  let documentPath = `${documentDirectory}/${documentFilename}`;
-  if (!documentPath.includes(".md")) {
-    documentPath = `${documentPath}/document.md`;
+  // If document is in a folder with assets
+  if (!documentFilename.includes(".md")) {
+    documentFilename = `${documentFilename}/document.md`;
   }
-  const content = await fs.readFile(documentPath, "utf8");
+  let content = await fs.readFile(documentFilename, "utf8");
+
+  // Extract the header from the file contents
   const match = content.match(/---\n((\w*:) .*\n)*---/)?.[0];
-  if (match) {
-    const post: Record<string, string> = {};
-    for (const [_, k, v] of match.matchAll(/^(?<key>\w*): (?<value>.*)$/gm)) {
-      post[k] = v;
-    }
-    const tags = post.tags?.split(",") || [];
-    if (post.title && post.description) {
-      return {
-        title: post.title,
-        description: post.description,
-        content: content.replace(match, ""),
-        date: `${year}-${month}-${day}`,
-        slug,
-        tags,
-      } as Document;
-    }
+  if (!match) throw `Error parsing the header for ${documentFilename}`;
+
+  // Remove the header from markdown file contents
+  content = content.replace(match, "");
+
+  const document: Record<string, string> = {};
+  // For each attribute in the header file add to the post object
+  for (const [_, k, v] of match.matchAll(/^(?<key>\w*): (?<value>.*)$/gm)) {
+    document[k] = v;
+  }
+  const tags = document.tags?.split(",") || [];
+  if (document.title && document.description) {
+    return {
+      title: document.title,
+      description: document.description,
+      content,
+      date: `${year}-${month}-${date}`,
+      slug,
+      tags,
+    } as Document;
   }
 }
