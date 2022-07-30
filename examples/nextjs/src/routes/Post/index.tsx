c8cb72e1425e0b2512fdc5d90e8901bf4df86573
Nicholas Romero <ncrmro@gmail.com>
2022-07-29 20:35:00 -0500

    chore(example/nextjs): clean up console log

--- examples/nextjs/src/routes/Post/index.tsx
+++ examples/nextjs/src/routes/Post/index.tsx
@@ -2,7 +2,6 @@ import { Post } from "../../types";
 import { Markdown } from "@quiescent/components";
 
 const PostRoute: React.FC<{ post: Post }> = (props) => {
-  console.log(props);
   return (
     <div>
       <h1>{props.post.title}</h1>
