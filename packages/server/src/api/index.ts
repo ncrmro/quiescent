f726dba46164641eba513e92f142ff2db172f2d1
Nicholas Romero <ncrmro@gmail.com>
2022-08-04 07:21:28 -0700

    chore(server): api logic

--- packages/server/src/api/index.ts
+++ packages/server/src/api/index.ts
@@ -1,7 +1,7 @@
-import documents from "../documents";
+import { getDocuments } from "../documents";
 
-export function getDocuments<REQ, RES>(req: REQ, res: RES) {
-  documents.get("posts", "dynamic");
+export async function apiGetDocuments<REQ, RES>(req: REQ, res: RES) {
+  const documents = await getDocuments("posts", "dynamic");
 }
 
 export default function quiescentAPI<REQ, RES>(req: REQ, res: RES) {}
