a77454b6056bdb3f02abd521b360711260beb60b
Nicholas Romero <ncrmro@gmail.com>
2022-08-07 19:35:25 +0000

    feat(examples/nextjs): render markdown with mdx

--- examples/nextjs/src/types.ts
+++ examples/nextjs/src/types.ts
@@ -1,5 +1,13 @@
 import { Document } from "@quiescent/server";
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
 
 export interface Post extends Document {
-  description: string;
+  state: "draft" | "published";
+}
+
+/**
+ * Used during server side and static props
+ */
+export interface MDXPost extends Omit<Post, "content"> {
+  content: MDXRemoteSerializeResult;
 }
